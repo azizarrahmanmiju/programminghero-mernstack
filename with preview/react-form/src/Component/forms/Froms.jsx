48c13e82c381333cf170904560adcfdb2841b60b
azizarrahman <azizarrahman01032003@gmail.com>
2024-11-16 15:44:42 +0600

    onchenged

--- with preview/react-form/src/Component/forms/Froms.jsx
+++ with preview/react-form/src/Component/forms/Froms.jsx
@@ -1,21 +1,31 @@
+import { useState } from "react";
 
 const Froms = () => {
 
+    const [name, setName] = useState(null);
+    const [password, setPassword] = useState(null);
+
     const onsubmit = e => {
         e.preventDefault()
-        console.log("submitted", e.target.name.value, e.target.password.value)
+        console.log(name, password)
     }
     const namechenge = e => {
-
-        console.log("chenged", e.target.value)
+        console.log(e.target.value);
+        setName(e.target.value)
     }
+    const passwordChege = e => {
+        console.log(e.target.value);
+        setPassword(e.target.value)
+    }
+
     return (
         <div>
             <form onSubmit={onsubmit}>
                 <input
                     onChange={namechenge}
                     type="text" name="name" ></input><br />
-                <input type="password" name="password" ></input><br />
+                <input
+                    onChange={passwordChege} type="password" name="password" ></input><br />
                 <input type="submit" />
             </form>
         </div>
