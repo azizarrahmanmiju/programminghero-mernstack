ab0bc11fc5f76cc09d3bd1ed2748cad18de89ccd
azizarrahman <azizarrahman01032003@gmail.com>
2024-11-16 16:01:16 +0600

    validation

--- with preview/react-form/src/Component/forms/Froms.jsx
+++ with preview/react-form/src/Component/forms/Froms.jsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 
 const Froms = () => {
 
+    const [validation, setValidation] = useState(false);
+
     const [name, setName] = useState(null);
     const [password, setPassword] = useState(null);
 
@@ -14,8 +16,14 @@ const Froms = () => {
         setName(e.target.value)
     }
     const passwordChege = e => {
-        console.log(e.target.value);
-        setPassword(e.target.value)
+        if (e.target.value.length < 6) {
+            setValidation(false)
+        } else {
+            setValidation(true)
+            console.log(e.target.value);
+            setPassword(e.target.value)
+        }
+
     }
 
     return (
@@ -28,6 +36,7 @@ const Froms = () => {
                     onChange={passwordChege} type="password" name="password" ></input><br />
                 <input type="submit" />
             </form>
+            <p>{(validation) ? "Validation True ✅" : "validation false ❌"}</p>
         </div>
     );
 };
