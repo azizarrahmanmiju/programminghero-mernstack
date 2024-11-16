5f8f7ee480a4a6aad1345c733a2a5444782d81f3
azizarrahman <azizarrahman01032003@gmail.com>
2024-11-16 14:46:57 +0600

    Form created

---
+++ with preview/react-form/src/App.jsx
@@ -0,0 +1,15 @@
+// import { useState } from 'react'
+
+import './App.css'
+import Froms from './Component/forms/froms'
+
+function App() {
+
+  return (
+    <>
+      <Froms></Froms>
+    </>
+  )
+}
+
+export default App
---
+++ with preview/react-form/src/Component/forms/Froms.jsx
@@ -0,0 +1,19 @@
+
+const Froms = () => {
+    return (
+        <div>
+            <form>
+                <p>Enter Name</p>
+                <input type="text" name="name"></input>
+                <p>Enter Email</p>
+                <input type="email" name="email"></input>
+                <p>Enter password</p>
+                <input type="password" name="password"></input>
+
+            </form>
+
+        </div>
+    );
+};
+
+export default Froms;
